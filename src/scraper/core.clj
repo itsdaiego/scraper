cd56c127c77356ba9cbfe58483b398fa29f6c655
Dyego Rodrigo <dyego9444@gmail.com>
2017-12-26 00:42:25 -0200

    get titles content

--- src/scraper/core.clj
+++ src/scraper/core.clj
@@ -5,13 +5,22 @@
 
 (use 'clojure.tools.trace)
 
-(defn get-fide-dom
+(defn get-chess-dom
   []
   (html/html-snippet
     (:body @(http/get "https://www.chess.com/articles" {:insecure? true}))))
 
+(defn get-articles-titles
+  [dom]
+  (html/select dom [:article.content :h2.title-featured :a]))
+
+(defn parse-content
+  [content]
+  (hash-map content :content))
+>>>>>>> 3945bcc... get titles content
+
 (defn -main
   ""
   [& args]
-  (let [content (get-fide-dom)]
-    (println ["result" content])))
+  (let [titles (parse-content(get-articles-titles(get-chess-dom)))]
+    (println titles)))
