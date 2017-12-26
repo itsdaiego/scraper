bba652b5837bd18431cb383bd922e8b254573705
Dyego Rodrigo <dyego9444@gmail.com>
2017-12-25 23:15:29 -0200

    get dom content from chess.com's website

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [enlive "1.1.6"]
+                 [org.clojure/tools.trace "0.7.9"]
+                 [http-kit "2.1.18"]]
   :main ^:skip-aot scraper.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/scraper/core.clj
+++ src/scraper/core.clj
@@ -1,7 +1,17 @@
 (ns scraper.core
+  (:require [net.cgrand.enlive-html :as html]
+            [org.httpkit.client :as http])
   (:gen-class))
 
+(use 'clojure.tools.trace)
+
+(defn get-fide-dom
+  []
+  (html/html-snippet
+    (:body @(http/get "https://www.chess.com/articles" {:insecure? true}))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  ""
   [& args]
-  (println "Hello, World!"))
+  (let [content (get-fide-dom)]
+    (println ["result" content])))
